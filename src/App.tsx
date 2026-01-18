2725460cd9c77882558a84ca68fe59a6ed860128
taboorech <karuselpopka@gmail.com>
2026-01-18 20:23:47 +0200

    refactor: remove SongDetails route

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ import ProfileEdit from './pages/profile-edit/profile-edit'
 import Subscriptions from './pages/subscriptions/subscriptions'
 import Lyrics from './pages/lyrics/lyrics'
 import LyricsTranslation from './pages/lyrics-translation/lyrics-translation'
-// import SongDetails from './pages/song-details/song-details'
 import SongEditor from './pages/song-editor/song-editor'
 
 const App = () => {
@@ -26,7 +25,6 @@ const App = () => {
           <Route path='/lyrics' element={<Lyrics />} />
           <Route path='/lyrics/translation' element={<LyricsTranslation />} />
           <Route path='/songs/new' element={<SongEditor />} />
-          {/* <Route path='/songs/:songId' element={<SongDetails />} /> */}
           <Route path='/songs/:songId/edit' element={<SongEditor />} />
         </Route>
       </Routes>
